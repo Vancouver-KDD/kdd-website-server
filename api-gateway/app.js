550a6d84c6e4021bb4eb7878013b0221a6b827f4
Jiin Kim <jiinkim@macbookpros-MacBook-Pro.local>
2021-07-25 10:59:58 -0700

    git ignore dbdata and env files
    Delete dbdata directory

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,4 +1,25 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
+const proxy = require('express-http-proxy');
 
 const app = express();
+app.use(cors());
+
+app.use('/users', proxy('localhost:4002', {
+  filter: function(req, res) {
+     return req.method == 'GET';
+  }
+}));
+
+app.use('/users', proxy('localhost:4004', {
+  filter: function(req, res) {
+     return req.method == 'POST';
+  }
+}));
+
+app.get("/", (req, res) => res.send("test"));
+
+app.listen(3000, () => {
+    console.log("Listening on 3000");
+});
